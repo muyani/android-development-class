d9747b4652a1ca4f24a87463d330c97c29f3d304
muyani <muyaniletina2@gmail.com>
2018-12-05 17:52:24 +0300

    first push

---
+++ src/com/letina/FirstLesson.java
@@ -0,0 +1,25 @@
+package com.letina;
+
+public class FirstLesson {
+    //just type psv it will suggest a main object for you.
+    public static void main(String[] args) {
+        //control space will make intelj to autosuggest.
+        //at the end of every java line,there is a semi colon.
+        System.out.println("Variables");
+        int age=70;
+        double weight=65.5;
+        String names="John Mark";
+        boolean isAlive =true;
+        double height =10;
+        System.out.println(""+ age +" "+ weight);
+        System.out.println(names+" "+weight);
+        float distance=100.99f;
+        short r=67;
+        long l=10000000;
+        char s = '~';
+        System.out.println(distance);
+
+
+    }
+
+}
---
+++ src/com/letina/Main.java
@@ -0,0 +1,16 @@
+
+//tells the program that this file belongs to this package.
+//this means you can have more than one main.java file same project but different package.
+package com.letina;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Word of Java");
+        System.out.println(77+45);
+        System.out.println(67/6+8-5*45);
+        System.out.println("My Simple Java Program");
+    }
+}
